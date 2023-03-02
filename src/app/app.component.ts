fd700f02b8ec7872203b4df8ff80271e63e5c66c
Thansingh <than.singh@interbizconsulting.com>
2023-03-02 18:10:51 +0530

    crud app is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserDataService } from 'src/services/user-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudApp';
+  userObj={
+    name: "",
+    email: "",
+    id: ''
+  }
+  usersData: any;
+  constructor(private userData: UserDataService){}
+  ngOnInit(){
+    this.displayUser();
+  }
+  getDetails(users:any){
+    console.log(users);
+    this.userData.createUser(users).subscribe(()=>{
+      this.displayUser();
+    });
+  }
+  displayUser(){
+    this.userData.getUsers().subscribe((res)=>{
+      this.usersData = res;
+    });
+  }
+
+  deleteData(user:any){
+    this.userData.deletUsers(user).subscribe(()=>{
+      this.displayUser();
+    });
+  }
+  editData(user:any){
+    this.userObj = user;
+  }
+
+  updateUser(){
+    this.userData.updateUsers(this.userObj).subscribe(()=>{
+      this.displayUser();
+    });
+  }
 }
